b019b9c2d5cdb58727a73b453f1975001037dbc4
Jason Petersen <jpetersen13@gmail.com>
2015-01-07 18:12:54 -0500

    Add homepage sidebar
    It looks good. I think I'll keep it.

--- index.php
+++ index.php
@@ -12,14 +12,29 @@
 	<body>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/top.php'; ?>
 		<div class="bio container">
-			<span class="img-with-wrap-left">
-				<img src="/images/headshot.jpg" alt="JP" class="img-circle" width="150" height="150">
-			</span>
-			<p>Way back when, I took my Film &amp; Media Arts degree from Philadelphia to Hollywood and worked as a software engineer for Warner Bros. and Sony Pictures.</p>
-			<p>Hard times during the <a href="http://en.wikipedia.org/wiki/Financial_crisis_of_2007%E2%80%9308" target="_blank">financial crisis</a> proved to be a boon for my wandering spirit: I traveled the world for no small amount of time. There are an <a href="https://www.flickr.com/photos/jasonpetersen/sets/" target="_blank">avalanche of photos</a> documenting it.</p>
-			<p>In Asia I became caught up in the Tibetan cause and followed my newfound interest in Tibetan Buddhism to Woodstock NY, home of world-renowned <a href="http://www.kagyu.org/" target="_blank">KTD Monastery</a>. I work there today, serving all IT and videography functions for the large community.</p>
-			<p>I'm also a web developer, dedicated to clean, mobile-first designs.</p>
-			<p>And I still make time for filmmaking pursuits&mdash;most recently having spent the prior summer editing a documentary in Thailand. <a href="https://vimeo.com/tormafilm/torma" target="_blank">Watch it here</a> (we could use the money).</p>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8">
+					<span class="img-with-wrap-left">
+						<img src="/images/headshot.jpg" alt="JP" class="img-circle" width="150" height="150">
+					</span>
+					<p>Way back when, I took my Film &amp; Media Arts degree from Philadelphia to Hollywood and worked as a software engineer for Warner Bros. and Sony Pictures.</p>
+					<p>Hard times during the <a href="http://en.wikipedia.org/wiki/Financial_crisis_of_2007%E2%80%9308" target="_blank">financial crisis</a> proved to be a boon for my wandering spirit: I traveled the world for no small amount of time. There are an <a href="https://www.flickr.com/photos/jasonpetersen/sets/" target="_blank">avalanche of photos</a> documenting it.</p>
+					<p>In Asia I became caught up in the Tibetan cause and followed my newfound interest in Tibetan Buddhism to Woodstock NY, home of world-renowned <a href="http://www.kagyu.org/" target="_blank">KTD Monastery</a>. I work there today, serving all IT and videography functions for the large community.</p>
+					<p>I'm also a web developer, dedicated to clean, mobile-first designs.</p>
+					<p>And I still make time for filmmaking pursuits&mdash;most recently having spent the prior summer editing a documentary in Thailand. <a href="https://vimeo.com/tormafilm/torma" target="_blank">Watch it here</a> (we could use the money).</p>
+					<hr class="visible-xs" />
+				</div>
+				<div class="col-xs-12 col-sm-3 col-sm-offset-1">
+					<h5><strong>WORK</strong></h5>
+					<p><a href="#">Full CV</a></p>
+					<br />
+					<h5><strong>CONTACT</strong></h5>
+					<p><a href="#">address@domain.com</a></p>
+					<br />
+					<h5><strong>SOCIAL</strong></h5>
+					<p><a href="#">Twitter</a></p>
+				</div>
+			</div>
 		</div>
 		<div class="spacer20"></div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/bottom.php'; ?>
