bf2c864868d6903402ab198d1d7c731a2e4f28e6
Jason Petersen <jpetersen13@gmail.com>
2015-01-07 15:45:47 -0500

    Header update

--- head.php
+++ head.php
@@ -1,8 +1,9 @@
 		<meta charset="utf-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="/css/main.css" />
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
+		<link href="/css/main.css" rel="stylesheet" />
 		<meta name="author" content="Jason Petersen" />
 		<meta name="keywords" content="web developer, videographer, IT, design" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
