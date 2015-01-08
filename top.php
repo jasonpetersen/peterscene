86e6e53dd206cd8e67353be28b821d9e2d779e2f
Jason Petersen <jpetersen13@gmail.com>
2015-01-08 16:39:16 -0500

    Add Mission page
    Still needs work, but it’s up.

--- top.php
+++ top.php
@@ -4,19 +4,19 @@
 					<a href="/"><img src="/images/banner-V1-small-indexed.gif" alt="Peterscene" width="200" height="57"></a>
 				</div>
 				<div class="col-xs-12 col-sm-8 col-md-4 col-lg-3">
-					<ul>
+					<ul role="menu">
 						<li><a href="/"><span class="current">Bio</span></a></li>
-						<li><a href="/"><span>Mission</span></a></li>
-						<li><a href="/"><span>Blog</span></a></li>
-						<li><a href="/"><span>Portfolio</span></a></li>
+						<li><a href="/mission"><span>Mission</span></a></li>
+						<li class="disabled"><a href="/blog"><span>Blog</span></a></li>
+						<li class="disabled"><a href="/portfolio"><span>Portfolio</span></a></li>
 						<br class="visible-sm visible-xs" />
-						<li><a href="/"><span>Video</span></a></li>
-						<li><a href="/"><span>Photos</span></a></li>
-						<li><a href="/"><span>Innovations</span></a></li>
+						<li class="disabled"><a href="/video"><span>Video</span></a></li>
+						<li class="disabled"><a href="/photos"><span>Photos</span></a></li>
+						<li class="disabled"><a href="/experiments"><span>Experiments</span></a></li>
 					</ul>
 				</div>
 				<div class="col-xs-12 col-sm-12 col-md-5 col-md-6">
-					<h4>The online residence of <strong>Jason&nbsp;Petersen</strong>, a&nbsp;web&nbsp;developer, videographer, IT&nbsp;professional, and&nbsp;wordsmith.</h4>
+					<h4>The online residence of <strong>Jason&nbsp;Petersen</strong>, a&nbsp;web&nbsp;developer, <br class="visible-sm" />videographer, IT&nbsp;professional, and&nbsp;wordsmith.</h4>
 				</div>
 			</div>
 			<br />
