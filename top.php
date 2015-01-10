e073637b54437c87cefb556d01e10328615e83a8
Jason Petersen <jpetersen13@gmail.com>
2015-01-10 08:42:37 -0500

    Banner style changes

--- top.php
+++ top.php
@@ -1,9 +1,9 @@
 		<div class="banner container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-4 col-md-3">
+				<div class="col-xs-12 col-sm-4 col-md-3 logo">
 					<a href="/"><img src="/images/banner-V1-small-indexed.gif" alt="Peterscene" width="200" height="57"></a>
 				</div>
-				<div class="col-xs-12 col-sm-8 col-md-4 col-lg-3">
+				<div class="col-xs-12 col-sm-8 col-md-4 col-lg-3 menu-options">
 					<ul role="menu">
 						<li><a class="bio" href="/">Bio</a></li>
 						<li><a class="purpose" href="/purpose">Purpose</a></li>
@@ -16,10 +16,9 @@
 					</ul>
 				</div>
 				<div class="col-xs-12 col-sm-12 col-md-5 col-md-6 logline">
-					<p>The online residence of <span class="stress">Jason Petersen</span>, a web developer, <br class="visible-sm" />videographer, IT professional, and wordsmith.</p>
+					<p>The online residence of <span class="stress">Jason Petersen</span>, a&nbsp;web developer, <br class="visible-sm" />videographer, IT professional, and wordsmith.</p>
 				</div>
 			</div>
-			<br />
-			<hr>
+			<hr />
 		</div>
 		<br />
