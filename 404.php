8ec95e6b4894645eb38084ce468e3050a529f618
Jason Petersen <jpetersen13@gmail.com>
2015-01-18 13:34:11 -0500

    Button color style changes

--- 404.php
+++ 404.php
@@ -4,6 +4,8 @@
 					<p>I might have goofed.</p>
 					<p>You might have goofed.</p>
 					<p>The net result is the same: all I've got for you is this silly drawing.</p>
+					<a class="btn btn-default" href="/" role="button">Homepage</a>
+					<div class="spacer20"></div>
 				</div>
 				<div class="col-xs-12 col-sm-7">
 					<img src="/images/404.gif" alt="404" class="img-responsive">
