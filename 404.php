dee165f2e380baa223780f8a161756f9186d31d8
Jason Petersen <jpetersen13@gmail.com>
2015-01-18 12:56:59 -0500

    Silly drawing for 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,12 @@
-		<div class="container spacious2">
-			<p>I might have goofed.</p>
-			<p>You might have goofed.</p>
-			<p>The net result is the same: there is no content here.</p>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-5 spacious2">
+					<p>I might have goofed.</p>
+					<p>You might have goofed.</p>
+					<p>The net result is the same: all I've got for you is this silly drawing.</p>
+				</div>
+				<div class="col-xs-12 col-sm-7">
+					<img src="/images/404.gif" alt="404" class="img-responsive">
+				</div>
+			</div>
 		</div>
