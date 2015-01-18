b72b0047efa132facaef15135b007d383b448300
Jason Petersen <jpetersen13@gmail.com>
2015-01-18 15:15:54 -0500

    Update index bucket
    Now handles new files that have not been explicitly referenced within
    index.php.

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php
+
+error_reporting(0);
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "jpete13_sql");
+define("DBPASSWORD", "C@3rlion");
+define("DBNAME", "jpete13_blog");
+define("USERTABLE", "entries");
+
+$mainTitle="The online residence of Jason Petersen";
+$defaultDesc="Web developer, videographer, photographer, wordsmith, world traveler, and all-around upstanding gentleman.";
+
+$errorTitle="Oops! | Jason Petersen";
+$errorDesc="Something went wrong.";
+
 if ($_GET['id'] == "") {
 	$template=true;
 	$id="bio";
-	$title="The online residence of Jason Petersen";
-	$desc="Web developer, videographer, photographer, wordsmith, world traveler, and all-around upstanding gentleman.";
+	$title=$mainTitle;
+	$desc=$defaultDesc;
 } else {
 	switch(strtolower($_GET['id'])) {
 		case "bio":
 			$template=true;
 			$id="bio";
-			$title="The online residence of Jason Petersen";
-			$desc="Web developer, videographer, photographer, wordsmith, world traveler, and all-around upstanding gentleman.";
+			$title=$mainTitle;
+			$desc=$defaultDesc;
 			break;
 		case "cv":
 			$template=false;
@@ -25,11 +40,6 @@ if ($_GET['id'] == "") {
 		case "blog":
 			$template=false;
 			$id="blog";
-			define("HOSTNAME", "localhost");
-			define("USERNAME", "jpete13_sql");
-			define("DBPASSWORD", "C@3rlion");
-			define("DBNAME", "jpete13_blog");
-			define("USERTABLE", "entries");
 			if ($_GET['entry'] != "") {
 				define("BLOGAVENUE", "entry");
 				define("BLOGENTRY", strtolower($_GET['entry']));
@@ -44,11 +54,25 @@ if ($_GET['id'] == "") {
 			$template=false;
 			$id="blog-add";
 			break;
-		default:
+		case "404":
 			$template=true;
 			$id="404";
-			$title="Oops! | Jason Petersen";
-			$desc="Something went wrong.";
+			$title=$errorTitle;
+			$desc=$errorDesc;
+			break;
+		default:
+			if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . strtolower($_GET['id']) . '.php')) {
+				$template=true;
+				$id=strtolower($_GET['id']);
+				$title=ucfirst(strtolower($_GET['id'])) . " | Jason Petersen";
+				$desc=$defaultDesc;
+			} else {
+				$template=true;
+				$id="404";
+				$title=$errorTitle;
+				$desc=$errorDesc;
+			}
+			break;
 	}
 }
 
