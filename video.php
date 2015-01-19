f6b99a4b81c502c61c02fa7b9ff0bc1eb9ac96b7
Jason Petersen <jpetersen13@gmail.com>
2015-01-19 17:41:53 -0500

    Minor change to video embed code

--- video.php
+++ video.php
@@ -1,8 +1,8 @@
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 spacious2">
+				<div class="col-lg-12 spacious2">
 					<div class="embed-responsive embed-responsive-16by9">
-						<iframe id="ytplayer" class="embed-responsive-item" type="text/html" src="http://www.youtube.com/embed/VWeyNW-gIrA?origin=http://peterscene.com"></iframe>
+						<iframe id="ytplayer" class="embed-responsive-item" type="text/html" src="http://www.youtube.com/embed/VWeyNW-gIrA"></iframe>
 					</div>
 				</div>
 			</div>
