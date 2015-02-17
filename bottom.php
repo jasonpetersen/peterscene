d21548763757e47fd25794d5199435eb9c95756a
Jason Petersen <jpetersen13@gmail.com>
2015-02-17 16:35:58 -0500

    Implement Back To Top functionality
    Add Back to Top link to footer.

--- bottom.php
+++ bottom.php
@@ -2,8 +2,9 @@
 			<div class="container">
 				<div class="hr-banner-footer"></div>
 				<span>&copy; 2015 Jason Petersen</span>
-				<span class="backToTop"><a href="#"><i class="fa fa-arrow-up"></i>&nbsp;Top</a></span>
+				<span id="backToTop"><a href="#"><i class="fa fa-arrow-up"></i>&nbsp;Top</a></span>
 			</div>
 		</footer>
 		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<script src="/js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 var main = function() {
-	
+	// The back-to-top link is hidden by default. Show it if the document size is 2x or more of the window size.
+	function displayTopLink() {
+		if (($(document).height() / $(window).height()) > 2) {
+			if ($('#backToTop').is(":hidden")) $('#backToTop').show();
+		} else {
+			if ($('#backToTop').is(":visible")) $('#backToTop').hide();
+		}
+	}
+	$(window).resize(displayTopLink);
+	displayTopLink();
+	// Animate the back-to-top action
+	$('#backToTop a').click(function (e) {
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
 };
 
 $(document).ready(main);
