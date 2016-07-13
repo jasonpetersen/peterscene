d057fa9c1244c448c32c909c6bbaf34799941b23
Jason Petersen <jpetersen13@gmail.com>
2016-07-13 12:09:57 -0400

    Content update
    New-ish projects, overdue for addition to the site.

--- experiments.php
+++ experiments.php
@@ -19,7 +19,8 @@ define("PAGEDESC", "A playground of code.");
 						<div class="col-xs-7 col-sm-8 col-md-9">
 							<h3>Anonynote</h3>
 							<p class="ital">A lightweight web app for maintaining and organizing notes.</p>
-							<p><a href="/experiments/anonynote">Launch app</a> <i class="fa fa-hand-o-right"></i></p>
+							<p><a href="https://anonynote.net" target="_blank">Go to anonynote.net</a> <i class="fa fa-external-link"></i></p>
+							<p><a href="/experiments/anonynote">Launch original in-site design</a> <i class="fa fa-hand-o-right"></i></p>
 						</div>
 						<div class="col-xs-5 col-sm-4 col-md-3 text-right">
 							<img src="/images/anonynote.gif" alt="Anonynote" class="img-thumbnail" style="max-height: 100px;">
@@ -28,10 +29,10 @@ define("PAGEDESC", "A playground of code.");
 					<div class="row spacious2">
 						<div class="col-xs-12">
 							<div class="hr-small"></div>
+							<p>This project started life on this website as a sort of proof-of-concept, but I decided I liked it enough that it needed to be <a href="https://anonynote.net" target="_blank">its own website</a>. I spent some months on it and am very happy with how it turned out. I hope it finds a user base!</p>
 							<p>The idea was to create a lightweight web app for maintaining and organizing notes&mdash;no logins, no passwords. Quick and easy. Amongst other uses, my thought was to have a space for storing unmemorizable textual information that I could quickly copy and paste from.</p>
 							<p>I was inspired by a website called <a href="http://www.dispostable.com/" target="_blank">Dispostable</a>, which I have long been a patron of. It allows you to access any email inbox at the dispostable.com domain&mdash;perfect as a substitute for giving a questionable site your real email. You cannot password protect these email accounts. Your only protection is that no one knows which account you're using. There's no directory. In essence, your email address <span class="ital">is</span> your password.</p>
 							<p>So it is with <span class="bold">Anonynote</span>. You create a notepad and fill it with notes. Color code it and arrange it to your taste. Anyone can open the same notepad you've created, but, there's no directory. No one knows your notepad name except you. As long as it isn't completely generic, the odds of someone stumbling upon it are slim.</p>
-							<p>I encourage you to try it, and if you like it, put it to use. I do!</p>
 							<div class="hr-small visible-xs"></div>
 						</div>
 					</div>
--- portfolio.php
+++ portfolio.php
@@ -20,12 +20,45 @@ define("PAGEDESC", "I am a web developer, dedicated to clean, mobile-first desig
 					</div>
 					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 						<div class="panel panel-default">
-							<a class="panel-heading-wrap" href="#project-1" data-toggle="collapse" data-parent="#accordion" aria-expanded="true" aria-controls="project-1">
+							<a class="panel-heading-wrap" href="#project-1" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="project-1">
 								<div class="panel-heading" role="tab" id="heading-1">
+									<h4 class="panel-title">Albany KTC</h4>
+								</div>
+							</a>
+							<div id="project-1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-1">
+								<div class="panel-body">
+									<div class="row">
+										<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+											<h4>Albany KTC</h4>
+											<ul class="no-bullet">
+												<li><span class="ital">New York-based non-profit</span></li>
+												<li>Location: Albany, NY</li>
+												<li>URL: <a href="http://albanyktc.org/" target="_blank">albanyktc.org&nbsp;<i class="fa fa-external-link"></i></a></li>
+												<li>Date: 2016</li>
+											</ul>
+										</div>
+										<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+											<div class="spacer20 visible-xs visible-sm"></div>
+											<img src="/images/albanyktc.jpg" class="img-responsive img-rounded" width="800" height="450" alt="albanyktc.org">
+										</div>
+									</div>
+									<div class="row spacious2">
+										<div class="col-xs-12">
+											<div class="hr-small"></div>
+											<p>Albany KTC, a non-profit Dharma center teaching authentic Tibetan Buddhism, contacted me about a website overhaul. They had gone through several web developers in the past, and what they were then operating with was a bizarre hybrid of a WordPress site and a great deal of custom code.</p>
+											<p>I sifted through everything and consolidated it under WordPress. This job was equal parts archaeology and janitorial work, as well as updating for responsive design. I enjoyed the challenge!</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<a class="panel-heading-wrap" href="#project-2" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="project-2">
+								<div class="panel-heading" role="tab" id="heading-2">
 									<h4 class="panel-title">Redline Motorsports</h4>
 								</div>
 							</a>
-							<div id="project-1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-1">
+							<div id="project-2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-2">
 								<div class="panel-body">
 									<div class="row">
 										<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -34,7 +67,7 @@ define("PAGEDESC", "I am a web developer, dedicated to clean, mobile-first desig
 												<li><span class="ital">Automotive parts and service</span></li>
 												<li>Location: North Wales, PA</li>
 												<li>URL: <a href="https://www.redlinemoto.com" target="_blank">redlinemoto.com&nbsp;<i class="fa fa-external-link"></i></a></li>
-												<li>Dates: 2012 - present</li>
+												<li>Dates: 2012 - 2015</li>
 											</ul>
 										</div>
 										<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -45,9 +78,9 @@ define("PAGEDESC", "I am a web developer, dedicated to clean, mobile-first desig
 									<div class="row spacious2">
 										<div class="col-xs-12">
 											<div class="hr-small"></div>
-											<p>I have managed the Redline Motorsports online identity since 2007, from branding, to design, to sales.</p>
-											<p>This latest incarnation of the website is built for e-commerce. To date, total sales exceed $45,000, and that is with a limited advertising budget. In addition, the success of this website has driven sales on other platforms, including Amazon and eBay.</p>
-											<p>The company continues to grow and I look forward to being a creative force behind their online expansion. The next step is integrating Redline's various business ventures into a single online hub, as well as redesigning for a mobile-first presentation.</p>
+											<p>I managed Redline Motorsports' online identity since 2007, from branding, to design, to sales.</p>
+											<p>This latest incarnation of the website was built for e-commerce. To date, total sales exceeded $45,000, and that was with a limited advertising budget. In addition, the success of this website drove sales on other platforms, including Amazon and eBay.</p>
+											<p>The company continues to grow and evolve. I am happy to have contributed to their success and look forwarded to seeing what they get into next.</p>
 										</div>
 									</div>
 								</div>
